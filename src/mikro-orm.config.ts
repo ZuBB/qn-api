9a410186948e38d065294f1386168bb35a9a3717
Vasyl Zuziak <zvasylvv@gmail.com>
2023-04-14 15:52:44 -0700

    feat: add stub of todos resourse

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,6 +2,7 @@ import { Options } from '@mikro-orm/core';
 import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
 
 const config: Options = {
+  // TODO: take filename from env vars
   dbName: 'hatch-todos.db',
   type: 'sqlite',
 

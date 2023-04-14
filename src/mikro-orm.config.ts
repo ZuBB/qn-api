c3e5f5ad882e270cb7d75765c1381a9ec520ae47
Vasyl Zuziak <zvasylvv@gmail.com>
2023-04-14 15:47:17 -0700

    chore: setup db. CLI part

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,21 @@
+import { Options } from '@mikro-orm/core';
+import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
+
+const config: Options = {
+  dbName: 'hatch-todos.db',
+  type: 'sqlite',
+
+  entitiesTs: ['./src/**/*.entity.*'],
+  entities: ['./dist/**/*.entity.*'],
+  migrations: {
+    path: './dist/migrations',
+    pathTs: './src/migrations',
+  },
+
+  forceUtcTimezone: true,
+  metadataProvider: TsMorphMetadataProvider,
+
+  debug: true,
+};
+
+export default config;

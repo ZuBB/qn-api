1199441130561abe975d7a33739c00c8d54b63f3
Vasyl Zuziak <zvasylvv@gmail.com>
2023-04-14 18:35:47 -0700

    chore: cleanup infra things

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   // TODO: take port from env vars
   await app.listen(8000);
 }
